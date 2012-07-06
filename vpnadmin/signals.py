975ae8cb8939b5ee127bc438c2d27935836efc8e
Vaclav Klecanda <vencax77@gmail.com>
2012-07-06 09:04:55 +0200

    prevent credit issues to ourself

--- vpnadmin/signals.py
+++ vpnadmin/signals.py
@@ -5,10 +5,16 @@ Created on Jun 25, 2012
 '''
 from invoices.models import CompanyInfo, Invoice, Item
 from django.utils.translation import ugettext
+from django.conf import settings
 
 def on_new_credit(sender, vs, ss, amount, creditInfo, **kwargs):
     """ Creates invoice with credit """
-    companyInfo = CompanyInfo.objects.get(user__id=int(vs))
+    userID = int(vs)
+    companyInfo = CompanyInfo.objects.get(user__id=userID)
+    
+    if userID == settings.OUR_COMPANY_ID:
+        return # do not generate invoice to ourself
+    
     invoice = Invoice(subscriber=companyInfo, paid=True)
     invoice.save()
     invoice.items.add(Item(price=amount, 
