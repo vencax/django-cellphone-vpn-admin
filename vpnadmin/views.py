e0f71e6decce6d00d7db6e7f851390ae36100ebc
Vaclav Klecanda <vencax77@gmail.com>
2012-10-09 09:09:58 +0200

    selectable date of processing

--- vpnadmin/views.py
+++ vpnadmin/views.py
@@ -33,19 +33,21 @@ FREE_SMS_RATIO = float(FREE_SMS_COUNT) / FREE_MINS_COUNT
 
 class BillUploadForm(forms.Form):
     bill = forms.FileField()
+    day = forms.DateField(initial=datetime.date.today())
     billdata = forms.CharField(widget=Textarea)
 
-def getBillFileName():
-    return datetime.date.today().strftime('%Y-%m.pdf')
+def getBillFileName(day):
+    return day.strftime('%Y-%m.pdf')
 
-def getBillFilePath():
-    return os.path.join(settings.MEDIA_ROOT, getBillFileName())
+def getBillFilePath(day):
+    return os.path.join(settings.MEDIA_ROOT, getBillFileName(day))
 
-def getBillUrl():
-    return '%s%s' % (settings.MEDIA_URL, getBillFileName())
+def getBillUrl(day):
+    return '%s%s' % (settings.MEDIA_URL, getBillFileName(day))
 
 SESSION_KEY = 'parsedInfo'
 DATA_SK = 'dataInfo'
+DAY_SK = 'day'
 
 class UploadBillView(FormView):
     template_name = 'vpnadmin/uploaddata.html'
@@ -61,7 +63,11 @@ class UploadBillView(FormView):
         stringStream = StringIO.StringIO(form.cleaned_data['billdata'])
         parsed = parser.parse(stringStream)
         # save the bill file
-        with open(getBillFilePath(), 'w') as f:
+        
+        day = form.cleaned_data['day']
+        self.request.session[DAY_SK] = day
+        
+        with open(getBillFilePath(day), 'w') as f:
             f.write(form.cleaned_data['bill'].read())
 
         self.request.session[SESSION_KEY] = parsed
@@ -130,7 +136,7 @@ class ProcessBillView(TemplateView):
         request.session[DATA_SK] = data
 
         return self.render_to_response({
-            'billurl' : getBillUrl(),
+            'billurl' : getBillUrl(request.session[DAY_SK]),
             'parsed' : data,
             'totals' : total,
             'expectedInvoicePrice' : expectedInvoicePrice
@@ -139,7 +145,7 @@ class ProcessBillView(TemplateView):
     def post(self, request, *args, **kwargs):
         data = request.session.get(DATA_SK)
         invoices = self._processParsedData(data)
-        billurl = getBillUrl()
+        billurl = getBillUrl(request.session[DAY_SK])
         self.processInvoices(invoices, billurl)
 
         message = _('''%(count)i records processed OK.
