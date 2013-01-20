5238dab6f4873e979a4e0241037aa0be0a4c2b15
Vaclav Klecanda <vencax77@gmail.com>
2013-01-20 18:41:42 +0100

    autocompilation of localestrings

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
+import os
+import subprocess
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
-print find_packages()
+
+class MyInstall(install):
+    def run(self):
+        projpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'vpnadmin')
+        print 'Generating MO files in %s' % projpath
+        subprocess.call(['django-admin.py', 'compilemessages'],
+                        cwd=projpath)
+        install.run(self)
 
 setup(
     name='vpn-admin',
@@ -11,4 +22,5 @@ setup(
     url='vxk.cz',
     packages=find_packages(),
     include_package_data=True,
+    cmdclass={'install': MyInstall},
 )
